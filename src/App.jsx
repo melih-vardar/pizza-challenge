5a91b33078511ca64e612800dc35ca356316a013
Melih Vardar <mvardar97@gmail.com>
2024-10-11 08:04:38 +0300

    assets-moved-to-public

--- src/App.jsx
+++ src/App.jsx
@@ -10,32 +10,32 @@ import HomeFooter from './components/HomeFooterComponents/HomeFooter'
 
 const headerNavList = [
   {
-    "imgURL": "assets/iteration-2-aseets/icons/1.svg",
+    "imgURL": "../public/assets/iteration-2-aseets/icons/1.svg",
     "alt": "Kore",
     "name": "YENİ! Kore",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/icons/2.svg",
+    "imgURL": "../public/assets/iteration-2-aseets/icons/2.svg",
     "alt": "Pizza",
     "name": "Pizza",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/icons/3.svg",
+    "imgURL": "../public/assets/iteration-2-aseets/icons/3.svg",
     "alt": "Burger",
     "name": "Burger",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/icons/4.svg",
+    "imgURL": "../public/assets/iteration-2-aseets/icons/4.svg",
     "alt": "Kızartmalar",
     "name": "Kızartmalar",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/icons/5.svg",
+    "imgURL": "../public/assets/iteration-2-aseets/icons/5.svg",
     "alt": "Fast food",
     "name": "Fast food",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/icons/6.svg",
+    "imgURL": "../public/assets/iteration-2-aseets/icons/6.svg",
     "alt": "Gazlı İçecek",
     "name": "Gazlı İçecek",
   }
@@ -43,37 +43,37 @@ const headerNavList = [
 
 const additionalPartNavList = [
   {
-    "imgURL": "assets/iteration-2-aseets/icons/1.svg",
+    "imgURL": "../public/assets/iteration-2-aseets/icons/1.svg",
     "alt": "Ramen",
     "name": "Ramen",
     "specialClass": "",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/icons/2.svg",
+    "imgURL": "../public/assets/iteration-2-aseets/icons/2.svg",
     "alt": "Pizza",
     "name": "Pizza",
     "specialClass": "ara-nav-span",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/icons/3.svg",
+    "imgURL": "../public/assets/iteration-2-aseets/icons/3.svg",
     "alt": "Burger",
     "name": "Burger",
     "specialClass": "",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/icons/4.svg",
+    "imgURL": "../public/assets/iteration-2-aseets/icons/4.svg",
     "alt": "French fries",
     "name": "French fries",
     "specialClass": "",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/icons/5.svg",
+    "imgURL": "../public/assets/iteration-2-aseets/icons/5.svg",
     "alt": "Fast food",
     "name": "Fast food",
     "specialClass": "",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/icons/6.svg",
+    "imgURL": "../public/assets/iteration-2-aseets/icons/6.svg",
     "alt": "Soft drinks",
     "name": "Soft drinks",
     "specialClass": "",
@@ -82,31 +82,31 @@ const additionalPartNavList = [
 
 const additionalPartCard = [
   {
-    "imgURL": "assets/iteration-2-aseets/pictures/food-1.png",
+    "imgURL": "../public/assets/iteration-2-aseets/pictures/food-1.png",
     "name": "Terminal Pizza",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/pictures/food-2.png",
+    "imgURL": "../public/assets/iteration-2-aseets/pictures/food-2.png",
     "name": "Position Absolute Acı Pizza",
   }, {
-    "imgURL": "assets/iteration-2-aseets/pictures/food-3.png",
+    "imgURL": "../public/assets/iteration-2-aseets/pictures/food-3.png",
     "name": "useEffect Tavuklu Burger",
   },
 ]
 
 const footerNavList = [
   {
-    "imgURL": "assets/iteration-2-aseets/footer/icons/icon-1.png",
+    "imgURL": "../public/assets/iteration-2-aseets/footer/icons/icon-1.png",
     "alt": "adress",
     "name": "341 Londonderry Road, Istanbul Türkiye"
   },
   {
-    "imgURL": "assets/iteration-2-aseets/footer/icons/icon-2.png",
+    "imgURL": "../public/assets/iteration-2-aseets/footer/icons/icon-2.png",
     "alt": "email",
     "name": "aciktim@teknolojikyemekler.com"
   },
   {
-    "imgURL": "assets/iteration-2-aseets/footer/icons/icon-3.png",
+    "imgURL": "../public/assets/iteration-2-aseets/footer/icons/icon-3.png",
     "alt": "phone",
     "name": "+90 216 123 45 67"
   },
@@ -122,27 +122,27 @@ const footerMenuList = [
 
 const footerInstagram = [
   {
-    "imgURL": "assets/iteration-2-aseets/footer/insta/li-0.png",
+    "imgURL": "../public/assets/iteration-2-aseets/footer/insta/li-0.png",
     "alt": "Instagram-1",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/footer/insta/li-1.png",
+    "imgURL": "../public/assets/iteration-2-aseets/footer/insta/li-1.png",
     "alt": "Instagram-2",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/footer/insta/li-2.png",
+    "imgURL": "../public/assets/iteration-2-aseets/footer/insta/li-2.png",
     "alt": "Instagram-3",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/footer/insta/li-3.png",
+    "imgURL": "../public/assets/iteration-2-aseets/footer/insta/li-3.png",
     "alt": "Instagram-4",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/footer/insta/li-4.png",
+    "imgURL": "../public/assets/iteration-2-aseets/footer/insta/li-4.png",
     "alt": "Instagram-5",
   },
   {
-    "imgURL": "assets/iteration-2-aseets/footer/insta/li-5.png",
+    "imgURL": "../public/assets/iteration-2-aseets/footer/insta/li-5.png",
     "alt": "Instagram-6",
   }
 ]
